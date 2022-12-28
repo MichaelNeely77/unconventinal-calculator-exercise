6798133bf95e54bc7a96916821171f802d9946c5
Michael Neely <michael@michaelneely.net>
2022-12-28 16:55:14 -0500

    Next lesson

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,14 +4,14 @@ let currentResult = defaultResultValue;
 
 function add(num1, num2) {
     const result = num1 + num2;
-    alert('The result is ' + result) ;
+    return result;
 }
 
-add(5,6);
-add(3,4);
+currentResult = add(5,6);
 
 
-currentResult = (currentResult + 10) * 3/2 -1;
+
+
 
 let calculationDescription = `(${defaultResultValue} + 10) * 3 /2 -1`;
 
