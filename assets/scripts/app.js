f8f17beadceccff0b07e3e97646c08fcd19309c6
Michael Neely <michael@michaelneely.net>
2022-12-28 16:49:31 -0500

    Next module

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,15 @@ const defaultResultValue = 0;
 
 let currentResult = defaultResultValue;
 
+function add(num1, num2) {
+    const result = num1 + num2;
+    alert('The result is ' + result) ;
+}
+
+add(5,6);
+add(3,4);
+
+
 currentResult = (currentResult + 10) * 3/2 -1;
 
 let calculationDescription = `(${defaultResultValue} + 10) * 3 /2 -1`;
