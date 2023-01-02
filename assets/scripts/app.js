2f9af96f40716b31088e3b42c49cd22b2dc8d89a
Michael Neely <michael@michaelneely.net>
2023-01-02 09:19:28 -0500

    Added the math functions to the buttons

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,49 @@
 const defaultResultValue = 0;
-
 let currentResult = defaultResultValue;
 
-function add() {
-    const calcDescription = `${currentResult} + ${userInput.value}`; 
-    currentResult = currentResult + parseInt(userInput.value);
+function getUserInput() {
+    return parseInt(userInput.value);
+}
+
+function createWriteLog(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
     outputResult(currentResult, calcDescription);
 }
 
-addBtn.addEventListener('click', add);
+function add() {
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    createWriteLog('+', initialResult, enteredNumber);
+}
 
+function subtract() {
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createWriteLog('-', initialResult, enteredNumber);
+}
+
+function multiply() {
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createWriteLog('*', initialResult, enteredNumber);
+}
+
+
+function divide() {
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createWriteLog('/', initialResult, enteredNumber);
+}
+
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
 
 
 
